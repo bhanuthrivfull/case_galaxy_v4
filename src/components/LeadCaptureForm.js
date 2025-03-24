5af9674c99d9a070c7aec30d1f5ee30e28ca3c31
bhanuthrivfull <bhanu.dova@thrivfulltek.com>
2025-03-24 11:19:19 +0530

    pattern updated

--- src/components/LeadCaptureForm.js
+++ src/components/LeadCaptureForm.js
@@ -100,7 +100,7 @@ const LeadCaptureForm = () => {
 
   // Validation patterns
   const patterns = {
-    name: /^[A-Z][a-zA-Z](?: [A-Z][a-zA-Z]){0,19}$/,
+    name: /^[A-Z][a-zA-Z](?:\s[a-zA-Z]+)$/,
     phone: /^(?!.*(\d)\1{6})[6-9]\d{9}$/,
   };
 
